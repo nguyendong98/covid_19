7d6d078f419c606730c1b2e9bbf8717e046630c4
nguyendong98 <namasd2001@gmail.com>
2020-04-26 19:48:50 +0700

    Updates

---
+++ precache-manifest.8bd21504ab6449448bbbd14758bef19d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251f94f9df482dab667c6e19bab1768e",
+    "url": "/covid_19/index.html"
+  },
+  {
+    "revision": "3ed212035a160676c434",
+    "url": "/covid_19/static/css/main.69d46764.chunk.css"
+  },
+  {
+    "revision": "30bc0970a57b3bebe25c",
+    "url": "/covid_19/static/js/2.7acc5e76.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/covid_19/static/js/2.7acc5e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed212035a160676c434",
+    "url": "/covid_19/static/js/main.41999395.chunk.js"
+  },
+  {
+    "revision": "b9ef4753bf926301f37e",
+    "url": "/covid_19/static/js/runtime-main.6b94d643.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_19/static/media/covid.d7265326.png"
+  }
+]);
